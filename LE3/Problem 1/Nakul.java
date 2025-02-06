4b6844213efc5e140e1a23d32aebf7584f9cd7e1
221B466 <221B466@juet.co.in>
2025-02-06 14:53:04 +0530

    Lab Exercise 3'

---
+++ LE3/Problem 1/Nakul.java
@@ -0,0 +1,3 @@
+public class Nakul extends Pandvas{
+    
+}
---
+++ LE3/Problem 1/Vikarn.java
@@ -0,0 +1,8 @@
+public class Vikarn extends Kaurav{
+      void obey(){
+        System.out.println(" vikarn is obedient"); 
+    }
+    void kind(){
+        System.out.println("vikarn is  kind"); 
+}
+}
